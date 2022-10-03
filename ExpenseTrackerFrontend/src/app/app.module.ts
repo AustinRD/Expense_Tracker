e122973a52d886ab233b892ab278be6cbac8ec31
Austin DiLeonardo <austinrd95@gmail.com>
2022-10-03 18:42:07 -0400

    Configure basic sidebar with component navigation

--- ExpenseTrackerFrontend/src/app/app.module.ts
+++ ExpenseTrackerFrontend/src/app/app.module.ts
@@ -1,26 +1,27 @@
-import { CurrencyPipe } from '@angular/common';
-import { HttpClientModule } from '@angular/common/http';
-
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { CurrencyPipe } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { Routes, RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { TransactionComponent } from './transaction/transaction.component';
 import { AddEditTransactionComponent } from './transaction/add-edit-transaction/add-edit-transaction.component';
-
-import { TransactionDataService } from './transaction-data.service';
 import { AddEditTypeComponent } from './transaction/add-edit-type/add-edit-type.component';
 import { AddEditCategoryComponent } from './transaction/add-edit-category/add-edit-category.component';
 import { TransactionTypeListComponent } from './transaction/transaction-type-list/transaction-type-list.component';
 import { TransactionCategoryListComponent } from './transaction/transaction-category-list/transaction-category-list.component';
 import { TransactionTableComponent } from './transaction/transaction-table/transaction-table.component';
 
+import { TransactionDataService } from './transaction-data.service';
+
 const routes: Routes = [
   { path: 'transactionTable', component: TransactionTableComponent },
   { path: 'transactionTypes', component: TransactionTypeListComponent},
-  { path: 'transactionCategories', component: TransactionCategoryListComponent}
+  { path: 'transactionCategories', component: TransactionCategoryListComponent},
+  // TODO: Create a default "dashboard" component, remove transaction component from the below route
+  { path: 'dashboard', component: TransactionComponent } 
 ];
 
 @NgModule({
